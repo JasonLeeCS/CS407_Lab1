b8cd076b0204dab521cc629ab68798ff6ca63a96
Jason Lee <jlee967@wisc.edu>
2023-09-15 15:35:03 -0500

    Test committ

---
+++ app/src/main/java/com/cs407/helloworld/MainActivity.java
@@ -0,0 +1,15 @@
+package com.cs407.helloworld;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        System.out.println(("Hello World"));
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
